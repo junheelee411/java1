0a6982b00094252210599a37f7c711a3f0eb0afd
JunheeLee <80439077+junheelee411@users.noreply.github.com>
2025-09-05 18:45:32 +0900

    dataAndOperation

---
+++ dataAndOperation/stringApp.java
@@ -0,0 +1,19 @@
+
+public class stringApp {
+
+	public static void main(String[] args) {
+		
+		// Character VS String
+		
+		System.out.println("HelloWorld"); //String
+		System.out.println("H"); //String
+		System.out.println('H'); //Character
+
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape(특수문자 임무 일시적 해방)
+		System.out.println("Hello \"World\"");
+	}
+
+}
